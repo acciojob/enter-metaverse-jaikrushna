df314f7541ef00930f94bafd27e39754049d9a2e
Jaikrishna Binnar <86294906+jaikrushna@users.noreply.github.com>
2026-03-28 13:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+var txt = document.getElementById("title");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=> {
+	txt.innerHTML = "<b>Entered Metaverse</b>";
+})
